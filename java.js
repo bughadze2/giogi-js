c6279d6e46b70078337e1e20b1dfc54b45915156
bughadze2 <bughadze.giorgi6@gmail.com>
2026-09-24 11:24:39 +0400

    change

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-filterSelection("all") 
+filterSelection("all") // Execute the function and show all columns
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("column");
@@ -10,7 +10,7 @@ function filterSelection(c) {
   }
 }
 
-// Show filtered elements
+
 function w3AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -35,7 +35,7 @@ function w3RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-
+// Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
